1ea17b563441af3021d508d23efdeea85796ca9e
Ray Lin <raytlin@gmail.com>
2015-05-05 18:04:52 -0700

    in ch 13 section 'multiple gesture recognizers'

--- TouchTracker/TouchTracker/BNRDrawView.m
+++ TouchTracker/TouchTracker/BNRDrawView.m
@@ -20,6 +20,13 @@
 
 @implementation BNRDrawView
 
+-(void)doubleTap:(UIGestureRecognizer*)gr{
+    NSLog(@"Recognized double tap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+}
+
 -(instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
@@ -27,6 +34,11 @@
         self.finishedLines = [[NSMutableArray alloc]init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
     }
     return self;
 }
