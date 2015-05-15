22743802134b694966be5939f142be6ac4a3f2f5
Ray Lin <raytlin@gmail.com>
2015-05-14 22:32:47 -0700

    middle of ch23 NSManagedObject section

---
+++ Homepwner/Homepwner/BNRImageTransformer.h
@@ -0,0 +1,13 @@
+//
+//  BNRImageTransformer.h
+//  Homepwner
+//
+//  Created by Ray Lin on 5/14/15.
+//  Copyright (c) 2015 BananaFoundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRImageTransformer : NSValueTransformer
+
+@end
